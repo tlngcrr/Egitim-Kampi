db2fd5e17722c87e79b0f6d9f1f0364090e57683
Tülin <gecertulin1349@gmail.com>
2024-10-09 09:42:51 +0300

    Add project files.

---
+++ 05_LoopsWithStars/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_LoopsWithStars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Alt alta 10 tane yıldız oluşturma
+            //for(int i=1;i<=10;i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+            #region Yan yana 10 tane yıldız oluşturma
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("*");
+            //}
+            #endregion
+            #region Alt alta 10 tne yıldız oluşturma her satırda 10 tane yıldız olsun
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine("************");
+            //}
+            #endregion
+        }
+    }
+}
